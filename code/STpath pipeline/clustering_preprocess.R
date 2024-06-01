19710f12ac20676700cfbd59ae165941bb141d6c
zhiningsui <zsui.academics@gmail.com>
2024-05-31 20:12:54 -0400

    update

---
+++ code/STpath pipeline/clustering_preprocess.R
@@ -0,0 +1,239 @@
+library(Seurat) 
+library(tidyverse)
+library(Matrix) 
+library(ggplot2)
+library(cowplot)
+theme_set(theme_cowplot()) 
+options(future.globals.maxSize = 1e9) 
+
+# Define directory paths
+raw_data_dir = "../../../st2image_data/He_et_al_2020/data/"
+processed_data_dir = "../../data/He_2020/"
+figure_dir = "../../figure/He_2020/"
+
+# Load processed metadata for spots with information, proportions and annotations.
+load(file.path(processed_data_dir, "prop_ann_meta.RData"))
+# Read ST data metadata
+st_meta <- read.csv(file.path(raw_data_dir, "metadata.csv"))
+
+# Get unique IDs for spots in ST data
+spots <- unique(prop_ann_meta$X) 
+
+# Function to process the count matrix of each sample and create Seurat object
+process_sample <- function(sample_meta) {
+  # Get sample ID
+  s1 <- paste0(sample_meta$patient, "_", sample_meta$replicate) 
+  print(s1)
+  
+  # Get the cancer subtype
+  type1 <- sample_meta$type
+  # Simplify subtype
+  type1 <- ifelse(type1 %in% c('Luminal_A', 'Luminal_B'), "ER+", 
+                  ifelse(type1 %in% c('HER2_luminal', 'HER2_non_luminal'), "HER2+", type1)) 
+  
+  # Read and preprocess count matrix
+  st_counts_s <- read.csv(file.path(raw_data_dir, sample_meta$count_matrix), sep = "\t") %>%
+    remove_rownames() %>%
+    column_to_rownames(var="X") %>%
+    t() %>%
+    as("CsparseMatrix") %>%
+    .[startsWith(rownames(.), "ENSG"),] # Keep only rows starting with "ENSG"
+  
+  # Select spots that are in ST data and belongs to this sample and subset the corresponding count matrix 
+  spots_s <- spots[startsWith(spots, s1)]
+  spots_s <- sapply(strsplit(spots_s, "_"), function(x) x[3])
+  st_counts_s <- st_counts_s[, spots_s]
+  
+  # Create Seurat object with spots in ST data
+  CreateSeuratObject(counts = st_counts_s, min.features = 100, project = s1)
+}
+
+# Function to create QC plots
+create_qc_plots <- function(obj, prefix) {
+  pdf(file.path(figure_dir, paste0(prefix, "_QC.pdf")), width = 24, height = 8)
+  
+  features <- c("nFeature_RNA", "nCount_RNA") # Features for QC plots
+  Idents(obj) <- "sid"
+  for (feature in features) {
+    p1 <- VlnPlot(obj, features = feature, ncol = 1, split.by = "subtype", cols = scales::hue_pal()(5)) + # Violin plots split by subtype
+      VlnPlot(obj, features = feature, ncol = 1, split.by = "lab") # Violin plots split by lab
+    
+    print(p1)
+  }
+  
+  # Scatter plots for QC
+  p2 <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", group.by = "subtype", split.by = "sid", ncol = 9, cols = scales::hue_pal()(5)) +
+    FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", group.by = "lab", split.by = "sid", ncol = 9)
+  
+  print(p2)
+  dev.off()
+}
+
+# Create a merged Seurat object for all samples ---------------------------
+
+# Process all samples and create Seurat object for each sample
+all_st_obj <- lapply(1:nrow(st_meta), function(i) process_sample(st_meta[i,]))
+# Merge Seurat objects into one over
+merged_st_obj <- merge(x=all_st_obj[[1]],y = c(all_st_obj)[-1],
+                       add.cell.ids = paste0(st_meta$patient, "_", st_meta$replicate))
+# Add metadata to merged Seurat object
+metadata <- merged_st_obj@meta.data %>%
+  dplyr::rename(sid = orig.ident) %>%
+  mutate(X = rownames(.),
+         pid = sapply(str_split(sid, "_"), `[[`, 1),
+         rid = sapply(str_split(sid, "_"), `[[`, 2),
+         subtype = plyr::mapvalues(pid, from = unique(st_meta[,c("type", "patient")])$patient, to = unique(st_meta[,c("type", "patient")])$type),
+         label = plyr::mapvalues(X, from = unique(prop_ann_meta[,c("X", "label")])$X, to = unique(prop_ann_meta[,c("X", "label")])$label),
+         lab = ifelse(rid %in% c("C1", "C2", "D1"), "lab_C1_C2_D1", "lab_D2_E1_E2"))
+# Assign metadata to Seurat object
+merged_st_obj@meta.data <- metadata 
+# Save the Seurat object
+saveRDS(merged_st_obj, file = file.path(processed_data_dir, "merged_st_obj_unfilt.rds")) # Save the Seurat object
+# Generate QC plots for unfiltered data
+create_qc_plots(merged_st_obj, "unfilt") 
+
+
+# Filter spots ------------------------------------------------------------
+
+# Gene detection filtering (filter out spots with abnormal gene detection)
+ggplot() +
+  geom_density(aes(x=log10(merged_st_obj$nFeature_RNA)))
+
+tmp <- merged_st_obj$nFeature_RNA
+mean(tmp>4000)
+mean(tmp<500)
+# Filter spots that have unique feature counts over 4000 or less than 500
+high_det <- WhichCells(merged_st_obj, expression = nFeature_RNA > 4000)
+low_det <- WhichCells(merged_st_obj, expression = nFeature_RNA < 500)
+filtered_cells <- setdiff(WhichCells(merged_st_obj), c(low_det, high_det))
+merged_st_obj_tmp <- subset(merged_st_obj, cells = filtered_cells)
+
+# Number of cells after filtering
+cbind(table(Idents(merged_st_obj)), table(Idents(merged_st_obj_tmp)))
+
+# Remove genes that are no longer expressed in any cell
+all_st_obj_filt <- list()
+for(l1 in Layers(merged_st_obj_tmp)){
+  s1 = gsub("counts.", "", l1)
+  print(s1)
+  filt_matrix <- GetAssayData(merged_st_obj_tmp, "RNA", layer = l1)
+  filtered_obj <- CreateSeuratObject(counts = filt_matrix, project = s1, min.cells = 3)
+  filtered_obj$orig.ident <- s1
+  all_st_obj_filt[[s1]] <- filtered_obj
+}
+merged_st_obj_filt <- merge(x=all_st_obj_filt[[1]], y = c(all_st_obj_filt)[-1])
+# Update metadata for filtered object
+merged_st_obj_filt@meta.data <- cbind(select(merged_st_obj_filt@meta.data, -c("orig.ident")), 
+                                      select(merged_st_obj@meta.data, -c("nCount_RNA", "nFeature_RNA"))[rownames(merged_st_obj_filt@meta.data), ])
+# Save the Seurat object 
+saveRDS(merged_st_obj_filt, file=file.path(processed_data_dir, "merged_st_obj_filt_spots.rds"))
+# Generate QC plots for filtered data
+create_qc_plots(merged_st_obj_filt, "filt_spots")
+
+
+# Get the list of genes that are expressed in more than 10% of spots in each sample. 
+pdf(file.path(figure_dir, "zero_proportions.pdf"), width = 8, height = 5)
+high_expr_genes <- list()
+low_expr_genes <- list()
+for(l1 in Layers(merged_st_obj_tmp)){
+  s1 = gsub("counts.", "", l1)
+  print(s1)
+  filt_matrix <- GetAssayData(merged_st_obj_tmp, "RNA", layer = l1)
+  zero_prop <- rowSums(filt_matrix == 0)/ncol(filt_matrix)
+  p <- ggplot() +
+    geom_histogram(aes(x=zero_prop), binwidth = 0.01) +
+    geom_vline(xintercept = 0.9, col = "red") +
+    geom_vline(xintercept = 0.8, col = "blue") +
+    labs(title = s1) +
+    annotate("text", x= 0.3, y=2500, col = "red",
+             label= paste0(sum(zero_prop < 0.9), " genes are expressed in more than 10% spots.")) + 
+    annotate("text", x = 0.3, y=2300, col = "blue",
+             label = paste0(sum(zero_prop < 0.8), " genes are expressed in more than 20% spots."))
+  print(p)
+  high_expr_genes[[l1]] <- rownames(filt_matrix)[zero_prop < 0.9]
+  low_expr_genes[[l1]] <- rownames(filt_matrix)[zero_prop >= 0.9]
+}
+dev.off()
+
+# Get intersection of genes that are expressed in more than 10% of spots in each sample.
+common_high_expr_genes <- Reduce(intersect, high_expr_genes)
+length(common_high_expr_genes)
+# Get intersection of genes that are not expressed in more than 90% of spots in each sample.
+common_low_expr_genes <- Reduce(intersect, low_expr_genes)
+length(common_low_expr_genes)
+
+
+# Filter genes 1 ----------------------------------------------------------
+
+# 1. Remove genes that are not expressed in more than 90% of spots for each sample.
+all_st_obj_filt <- list()
+for(l1 in Layers(merged_st_obj_tmp)){
+  s1 = gsub("counts.", "", l1)
+  print(s1)
+  filt_matrix <- GetAssayData(merged_st_obj_tmp, "RNA", layer = l1)
+  filtered_obj <- CreateSeuratObject(counts = filt_matrix, project = s1, min.cells = 0.1*ncol(filt_matrix))
+  filtered_obj$orig.ident <- s1
+  all_st_obj_filt[[s1]] <- filtered_obj
+}
+merged_st_obj_filt <- merge(x=all_st_obj_filt[[1]], y = c(all_st_obj_filt)[-1])
+# Update metadata for filtered object
+merged_st_obj_filt@meta.data <- cbind(select(merged_st_obj_filt@meta.data, -c("orig.ident")), 
+                                      select(merged_st_obj@meta.data, -c("nCount_RNA", "nFeature_RNA"))[rownames(merged_st_obj_filt@meta.data), ])
+# Save the Seurat object 
+saveRDS(merged_st_obj_filt, file=file.path(processed_data_dir, "merged_st_obj_filt_zeros_per_sample.rds"))
+# Generate QC plots for filtered data
+create_qc_plots(merged_st_obj_filt, "filt_zeros_per_sample")
+
+# Filter genes 2 ----------------------------------------------------------
+
+# 2. Remove genes that are not expressed in more than 90% of spots in at least one sample.
+all_st_obj_filt <- list()
+for(l1 in Layers(merged_st_obj_tmp)){
+  s1 = gsub("counts.", "", l1)
+  print(s1)
+  filt_matrix <- GetAssayData(merged_st_obj_tmp, "RNA", layer = l1)
+  filt_matrix <- filt_matrix[common_high_expr_genes,]
+  filtered_obj <- CreateSeuratObject(counts = filt_matrix, project = s1, min.cells = 0)
+  filtered_obj$orig.ident <- s1
+  all_st_obj_filt[[s1]] <- filtered_obj
+}
+merged_st_obj_filt <- merge(x=all_st_obj_filt[[1]], y = c(all_st_obj_filt)[-1])
+# Update metadata for filtered object
+merged_st_obj_filt@meta.data <- cbind(select(merged_st_obj_filt@meta.data, -c("orig.ident")), 
+                                      select(merged_st_obj@meta.data, -c("nCount_RNA", "nFeature_RNA"))[rownames(merged_st_obj_filt@meta.data), ])
+# Save the Seurat object 
+saveRDS(merged_st_obj_filt, file=file.path(processed_data_dir, "merged_st_obj_filt_common_high_expr_genes.rds"))
+# Generate QC plots for filtered data
+create_qc_plots(merged_st_obj_filt, "filt_common_high_expr_genes")
+
+# Filter genes 3 ----------------------------------------------------------
+
+# 3. Remove genes that are not expressed in more than 90% of all pooled spots
+merged_st_obj_tmp <- JoinLayers(merged_st_obj_tmp)
+filt_matrix <- GetAssayData(merged_st_obj_tmp, "RNA", layer = "counts")
+zero_prop <- rowSums(filt_matrix == 0)/ncol(filt_matrix)
+
+pdf(file.path(figure_dir, "zero_proportions_all.pdf"), width = 8, height = 5)
+ggplot() +
+  geom_histogram(aes(x=zero_prop), binwidth = 0.01) +
+  geom_vline(xintercept = 0.9, col = "red") +
+  geom_vline(xintercept = 0.8, col = "blue") +
+  labs(title = "All Samples") +
+  annotate("text", x= 0.3, y=9000, col = "red",
+           label= paste0(sum(zero_prop < 0.9), " genes are expressed in more than 10% spots.")) + 
+  annotate("text", x = 0.3, y=8500, col = "blue",
+           label = paste0(sum(zero_prop < 0.8), " genes are expressed in more than 20% spots."))
+
+dev.off()
+
+filtered_obj <- CreateSeuratObject(counts = filt_matrix, project = "all", min.cells = 0.1*ncol(filt_matrix))
+# Update metadata for filtered object
+filtered_obj@meta.data <- cbind(select(filtered_obj@meta.data, -c("orig.ident")), 
+                                select(merged_st_obj@meta.data, -c("nCount_RNA", "nFeature_RNA"))[rownames(merged_st_obj_filt@meta.data), ])
+filtered_obj[["RNA"]] <- split(filtered_obj[["RNA"]], f = filtered_obj$sid)
+merged_st_obj_filt <- filtered_obj
+# Save the Seurat object 
+saveRDS(merged_st_obj_filt, file=file.path(processed_data_dir, "merged_st_obj_filt_zeros_all.rds"))
+# Generate QC plots for filtered data
+create_qc_plots(merged_st_obj_filt, "filt_zeros_all")
+
---
+++ code/STpath pipeline/create_patches.py
@@ -0,0 +1,252 @@
+"""
+Date last modified: May. 31, 2024 by Zhining Sui
+Program: create_patches.py
+Purpose: Create patches with exactly one filtered spot in each patch.
+-------------------------------------------------------------------------------
+How to Use This Script:
+1. **Data Inputs**: 
+    - **Input CSV file**: 
+        - The script reads input data from a CSV file which specifies the paths to required files for each WSI.
+        - The expected columns in the CSV file are:
+            - `sample_id`: Unique identifier for each sample/WSI.
+            - `image_path`: Path to the full-resolution image file (`*.tif`).
+            - `diameter`: Diameter of the spots (can be a path to `scalefactors_json.json` file, numbers, or empty).
+            - `spatial_path`: Path to the spatial pixel coordinates file.
+            - `barcode_path`: Path to the barcodes file, or to the filtered matrix if there is no barcodes file.
+            - `output_dir`: Directory where the patches will be saved.
+            - `barcode_col`: Column number for the barcode in `spatial_path`.
+            - `array_row_col`: Column number for the row position in `spatial_path` (optional).
+            - `array_col_col`: Column number for the column position in `spatial_path` (optional).
+            - `pxl_x_col`: Column number for the x pixel coordinate in `spatial_path`.
+            - `pxl_y_col`: Column number for the y pixel coordinate in `spatial_path`.
+2. **Data Outputs**: 
+    - Patches of the filtered spots are saved in `output_dir`, either in JPG or TIFF format.
+3. **Adjusting Patch Radius**:
+    - Modify the global parameter `FACTOR` to adjust the radius of the patches relative to the radius of the spots. 
+    - For 10X Visium, the radius of a spot is 27.5µm, and the distance between the centers of two spots should be 100µm. So, `FACTOR` can be at most 1.8 to avoid overlap between patches.
+4. **Coordinate Handling**:
+    - Ensure the pixel coordinates in the `spatial_path` CSV file are correctly assigned to the x and y axes. 
+    - Check the generated `spots_{sample_id}.jpg` in `output_dir` to verify if the crosses match the WSI. Adjust the coordinate assignment if necessary.
+    - For 10X Visium, the 5th and 6th columns in `tissue_positions_list.csv` are the pixel coordinates of the y and x axes, respectively. The coordinate system for the Python Imaging Library has the origin at the upper left corner. The x-axis (horizontal) increases from left to right, and the y-axis (vertical) increases from top to bottom. 
+    - For 10X Visium, the 3rd and 4th columns in `tissue_positions_list.csv` are the row and column positions of the spot, respectively. Ensure the detected image has the correct orientation (hourglass at the upper left corner, triangle at the lower left corner, filled hexagon at the upper right corner, and open hexagon at the lower right corner). 
+    - To locate spots in an image using pixel coordinates, use the `locate_pixels()` function.
+
+Notes:
+    - For detailed information on fiducial alignment for spatial transcriptomics, visit:
+      https://support.10xgenomics.com/spatial-gene-expression/software/visualization/latest/alignment#id_fiducials
+-------------------------------------------------------------------------------
+"""
+
+import numpy as np
+import tifffile as tiff
+from PIL import Image
+import json
+import os
+import pandas as pd
+import gzip
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Global parameter
+FACTOR = 1.1  # radius of patches/radius of spots
+
+# function to put crosses on the image with specific coordinates
+def locate_pixels(sample_id, image_path, pts, save_dir):
+
+    Image.MAX_IMAGE_PIXELS = None 
+    image = Image.open(image_path)
+    image_np = np.array(image)
+    
+    width, height, channel = image_np.shape
+
+    plt.imshow(image_np)
+    # plt.plot(0, 0, "og", markersize=5)  # og:shorthand for green circle
+    # plt.plot(width, height, "og", markersize=5)  # og:shorthand for green circle
+    plt.scatter(pts[:, 0], pts[:, 1], marker="x", color="red", s=10)
+    plt.title(sample_id)
+    
+    plt.savefig(f'{save_dir}/spots_{sample_id}.jpg', dpi=300)
+    plt.show()
+    
+
+def rgba2rgb(rgba, background=(255, 255, 255)):
+    """Convert RGBA image to RGB."""
+    row, col, ch = rgba.shape
+    if ch == 3:
+        return rgba
+    assert ch == 4, 'RGBA image has 4 channels.'
+    rgb = np.zeros((row, col, 3), dtype='float32')
+    a = rgba[:, :, 3] / 255.0
+    for i in range(3):
+        rgb[:, :, i] = rgba[:, :, i] * a + (1.0 - a) * background[i]
+    return rgb.astype('uint8')
+
+def read_sample_ids(file_path):
+    """Read sample IDs from a text file."""
+    with open(file_path, 'r') as file:
+        sample_ids = file.read().splitlines()
+    return sample_ids
+
+def find_radius(coords_fn = None, coords = None, diff_plot=False):
+    
+    # Load coordinates file
+    if coords is None:
+        if coords_fn.endswith(".gz"):
+            with gzip.open(coords_fn, 'rt') as f:
+                coords = pd.read_csv(f)
+        else:
+            coords = pd.read_csv(coords_fn)
+    
+    # Rename columns and split spot column into row and col
+    coords.rename(columns={coords.columns[0]: "barcode"}, inplace=True)
+    coords[['array_row', 'array_col']] = coords['barcode'].str.split('x', expand=True).astype(int)
+ 
+    adjacent_coords = pd.DataFrame(columns=["same", "spot_1", "spot_2", "dx", "dy"])  
+    for axis in ['array_row', 'array_col']:
+        unique_vals = coords[axis].unique()
+        for val in unique_vals:
+            group = coords[coords[axis] == val]
+            sorted_group = group.sort_values(by=['array_col' if axis == 'array_row' else 'array_row'])
+            if len(group) > 1:
+                for i in range(len(sorted_group.index)-1):
+                    if (sorted_group.iat[i+1, 4 if axis == 'array_row' else 3] - sorted_group.iat[i, 4 if axis == 'array_row' else 3]) == 1:
+                        adjacent_coords.loc[len(adjacent_coords.index)] = [axis, 
+                                                                           sorted_group.iat[i, 0], 
+                                                                           sorted_group.iat[i+1, 0], 
+                                                                           (sorted_group.iat[i+1, 1] - sorted_group.iat[i, 1]), 
+                                                                           (sorted_group.iat[i+1, 2] - sorted_group.iat[i, 2])]
+    mean_diff = adjacent_coords.groupby('same', as_index=False)[['dx', 'dy']].mean().assign(sample=coords_fn)
+    var_diff = adjacent_coords.groupby('same', as_index=False)[['dx', 'dy']].var().assign(sample=coords_fn)
+    
+    if diff_plot:
+        plot_diff(adjacent_coords, coords_fn)
+    
+    return coords, mean_diff, var_diff
+
+def plot_diff(adjacent_coords, coords_fn):
+    adjacent_coords = adjacent_coords.reset_index()
+    adjacent_coords_long = pd.melt(adjacent_coords, id_vars=['index', 'same'], value_vars=['pxl_x', 'pxl_y'])
+
+    plt.figure()
+    sns.relplot(
+        data=adjacent_coords_long, x="index", y="value",
+        col="variable", hue="same", style="same",
+        kind="scatter"
+    ).set(title=coords_fn)
+    plt.show()
+    
+    
+def create_patches(samples_df, output_format = 'jpg'):
+    for index, row in samples_df.iterrows():
+        sample_id = row['sample_id']
+        image_path = row['image_path']
+        diameter = row['diameter']
+        spatial_path = row['spatial_path']
+        barcode_path = row['barcode_path']
+        output_dir = row['output_dir']
+        barcode_col = row['barcode_col']
+        array_row_col = row['array_row_col']
+        array_col_col = row['array_col_col']
+        pxl_x_col = row['pxl_x_col']
+        pxl_y_col = row['pxl_y_col']
+        
+        try:
+            # load spatial information
+            sp = pd.read_csv(spatial_path, header=None, index_col=None)
+            sp.columns = [f'col_{i+1}' for i in range(sp.shape[1])]
+            rename_dict = {
+               f'col_{barcode_col}': 'barcode',
+               f'col_{pxl_x_col}': 'pxl_x',
+               f'col_{pxl_y_col}': 'pxl_y'
+            }
+            if pd.notna(array_row_col):
+                rename_dict[f'col_{array_row_col}'] = 'array_row'
+            if pd.notna(array_col_col):
+                rename_dict[f'col_{array_col_col}'] = 'array_col'
+            sp = sp.rename(columns=rename_dict)
+
+            # Subset the DataFrame to only include the renamed columns
+            sp = sp[list(rename_dict.values())].dropna()   
+
+            # Ensure correct data types
+            sp['barcode'] = sp['barcode'].astype(str)
+            sp['pxl_x'] = sp['pxl_x'].astype(float)
+            sp['pxl_y'] = sp['pxl_y'].astype(float)
+            if 'array_row' in sp.columns:
+                sp['array_row'] = sp['array_row'].astype(int)
+            if 'array_col' in sp.columns:
+                sp['array_col'] = sp['array_col'].astype(int)
+            
+
+            # Read diameter from JSON file or calculate if not available
+            if pd.isna(diameter):
+                sp, mean_diff, var_diff = find_radius(coords = sp) 
+                # Calculate diameter: ST data has 200 µm center-to-center distance between spots, each with a diameter of 100 µm. 
+                diameter = max( mean_diff[["dx", "dy"]].max(axis=1) ) / 2 
+                samples_df.at[index, 'diameter'] = diameter
+            elif isinstance(diameter, str) and diameter.endswith('.json'):
+                try:
+                    with open(diameter, 'r') as f:
+                        diameter_data = json.load(f)
+                        diameter = diameter_data['spot_diameter_fullres']
+                except FileNotFoundError:
+                    print(f'{diameter} not found.')
+            else:
+                diameter = diameter
+            
+            
+            # load barcodes for the filtered matrix (in-tissue spots)
+            barcodes_df = pd.read_csv(gzip.open(barcode_path, 'rt'), sep='\t', header=None, index_col=None)
+            if barcodes_df.shape[1] == 1:
+                barcodes = barcodes_df[0].tolist()
+            else: # if there is no separate barcodes file, but only has the filtered matrix file.
+                barcodes = [x for x in barcodes_df[0].tolist() if str(x) != 'nan']
+            # extract spatial information of the filtered spots by matching barcodes
+            sp_filtered = sp[sp["barcode"].isin(barcodes)]
+            
+            os.makedirs(output_dir, exist_ok=True)
+            
+            # check if the coordinates of spots correspond correctly with the image (image in correct orientation)
+            PTS = np.array(sp_filtered[["pxl_x", "pxl_y"]])
+            locate_pixels(sample_id = sample_id, image_path=image_path, pts = PTS, save_dir = output_dir)
+            
+            # get WSI
+            Image.MAX_IMAGE_PIXELS = None
+            slide = Image.open(image_path)
+            slide_rgb = rgba2rgb(np.array(slide))
+    
+            for _, spot_row in sp_filtered.iterrows():
+                # coordinates of the center of spots
+                centerX, centerY = spot_row['pxl_x'], spot_row['pxl_y']
+                # set the radius of the patch 
+                patch_radius = diameter * FACTOR / 2
+                # Crop the spot:
+                x, y = centerX - patch_radius, centerY - patch_radius
+                cropped_img = slide_rgb[round(y):round(y + 2 * patch_radius), round(x):round(x + 2 * patch_radius)]
+                
+                if output_format == "tiff":
+                    tiff.imwrite(f'{output_dir}/{sample_id}_{spot_row["barcode"]}.tif', cropped_img)
+                elif output_format == "jpg":
+                    Image.fromarray(cropped_img).save(f'{output_dir}/{sample_id}_{spot_row["barcode"]}.jpg', "JPEG", quality=100)
+                print(f'Created patch for spot: {sample_id}_{spot_row["barcode"]} in {output_format} format')
+        except Exception as e:
+            print(f'Error processing sample {sample_id}: {e}')
+    
+def main():
+    INPUT_CSV = '../../data/Wu_2021/create_patches_input.csv'  
+    # INPUT_CSV = '../../data/He_2020/create_patches_input.csv' 
+    # NPUT_CSV = '../../data/10X/create_patches_input.csv'  
+    
+    samples_df = pd.read_csv(INPUT_CSV)
+    
+    required_columns = ['sample_id', 'image_path', 'diameter', 'spatial_path', 'barcode_path', 'output_dir',
+                        'barcode_col', 'array_row_col', 'array_col_col', 'pxl_y_col', 'pxl_x_col']
+    for col in required_columns:
+        if col not in samples_df.columns:
+            samples_df[col] = np.nan
+
+    create_patches(samples_df, 'jpg')
+
+    
+if __name__ == '__main__':
+    main()
