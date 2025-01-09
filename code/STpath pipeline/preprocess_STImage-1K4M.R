fc65f0f72ca4054e968220d12bda2b95442a96a2
zhiningsui <zsui.academics@gmail.com>
2025-01-08 21:16:07 -0500

    update

--- code/STpath pipeline/preprocess_STImage-1K4M.R
+++ code/STpath pipeline/preprocess_STImage-1K4M.R
@@ -6,9 +6,15 @@ output_dir <- "../../output/STimage-1K4M/deconvolution/ST"
 
 # Create input files for create_patches.py and deconvolution -------------------
 
-metadata <- read.csv(file.path(github_dir, "metadata_brca.csv"))[,-1]
-metadata$sid <- metadata$slide
-sp_info <- metadata[, c("sid", "subtype", "tech")]
+metadata <- read.csv(file.path(github_dir, "metadata_brca.csv"))
+metadata_visium <- metadata[metadata$tech == "Visium", ]
+metadata_visium$subtype <- ifelse(metadata_visium$subtype %in% c("ER", "HER2"),
+                                  paste0(metadata_visium$subtype, "+"), 
+                                  metadata_visium$subtype)
+write.csv(metadata_visium, file = file.path(github_dir, "metadata_brca_visium.csv"),
+          row.names = F)
+metadata_visium$sid <- metadata_visium$slide
+sp_info <- metadata_visium[, c("sid", "subtype", "tech")]
 sp_info$dir <- file.path(st_dir, sp_info$tech)
 sp_info$spatial_path <- file.path(sp_info$dir, "coord", paste0(sp_info$sid, "_coord.csv"))
 all(file.exists(sp_info$spatial_path))
@@ -25,10 +31,10 @@ for (id in sp_info$sid ) {
   sp_info[sp_info$sid == id, "diameter"] <- unique(sp$r) * 2
 }
 sp_info$count_matrix_dir <- file.path(sp_info$dir, "gene_exp", paste0(sp_info$sid, "_count.csv"))
-sp_info$subtype <- ifelse(sp_info$subtype %in% c("ER", "HER2"), paste0(sp_info$subtype, "+"), sp_info$subtype)
 write.csv(sp_info, 
           file.path(github_dir, "create_patches_input_brca.csv"),
           row.names = F)
+
 
 directory <- "/Users/zhiningsui/GitHub/STimage-1K4M/Visium/image"
 all_files <- list.files(directory)
