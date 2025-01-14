d4952f13e728b86976080cb874a30ecfcbd4ee29
zhiningsui <zsui.academics@gmail.com>
2025-01-13 23:49:29 -0500

    update

--- code/STpath pipeline/download_STimage-1k4M.py
+++ code/STpath pipeline/download_STimage-1k4M.py
@@ -42,5 +42,56 @@ for file_path in file_paths:
 
 
     
-    
-    
+import os
+from synapseclient import Synapse
+import pandas as pd
+syn = Synapse()
+syn.login(authToken='eyJ0eXAiOiJKV1QiLCJraWQiOiJXN05OOldMSlQ6SjVSSzpMN1RMOlQ3TDc6M1ZYNjpKRU9VOjY0NFI6VTNJWDo1S1oyOjdaQ0s6RlBUSCIsImFsZyI6IlJTMjU2In0.eyJhY2Nlc3MiOnsic2NvcGUiOlsidmlldyIsImRvd25sb2FkIiwibW9kaWZ5Il0sIm9pZGNfY2xhaW1zIjp7fX0sInRva2VuX3R5cGUiOiJQRVJTT05BTF9BQ0NFU1NfVE9LRU4iLCJpc3MiOiJodHRwczovL3JlcG8tcHJvZC5wcm9kLnNhZ2ViYXNlLm9yZy9hdXRoL3YxIiwiYXVkIjoiMCIsIm5iZiI6MTczNjgwNzg2NCwiaWF0IjoxNzM2ODA3ODY0LCJqdGkiOiIxNTQyNiIsInN1YiI6IjM1Mjc4OTYifQ.i4lW4HRwD0uIAX_oV0XTnwI0oSJk75FiiqbqtJdggU922FYWoGsctkHyYc_lGq9wjdVhhkwVRuV_mNp0rmg9ecGd6VfzlMDQZm6CBAlB0nowHcy8B5TERRgFY2rq8RoQ8TPV-89iCVYsxiyRqeUjFPOrOoL4lY_FkS-OkCiUDVZ-fbrI5CIhji5JOul7HPuDbfAeB0yHqEklat-a1L13N0EzOpFR5F3hf_qFFU1llubtWG2D33les3bZAcekTHMv8x-C3gKz0zJzxd5tW-VapGiUw_yqZgBBsPG36Pv5ab1PWfbks4cIp1h437baoVG4yE-o6LnXeb3kDn9Fy11DOQ')
+
+# Load the full list of Synapse IDs from the CSV file
+file_path = "/Users/zhiningsui/GitHub/STpath/code/STpath pipeline/Unsuccessful_Downloads.csv"
+synapse_ids = pd.read_csv(file_path)["SynapseID"].tolist()
+
+download_directory = "/Users/zhiningsui/GitHub/Mo_2024"  # Replace with your desired path
+os.makedirs(download_directory, exist_ok=True)  # Create the directory if it doesn't exist
+
+# Track unsuccessful downloads
+unsuccessful_downloads = []
+
+# Attempt to download each file
+for syn_id in synapse_ids:
+    try:
+        print(f"Downloading {syn_id} to {download_directory}...")
+        syn.get(syn_id, downloadLocation=download_directory)
+        print(f"Successfully downloaded {syn_id}.")
+    except Exception as e:
+        print(f"Error downloading {syn_id}: {e}")
+        unsuccessful_downloads.append({"SynapseID": syn_id, "Error": str(e)})
+
+# Save the list of unsuccessful downloads to a CSV file
+if unsuccessful_downloads:
+    error_file_path = "/Users/zhiningsui/GitHub/STpath/code/STpath pipeline/Unsuccessful_Downloads.csv"
+    pd.DataFrame(unsuccessful_downloads).to_csv(error_file_path, index=False)
+    print(f"List of unsuccessful downloads saved to {error_file_path}.")
+else:
+    print("All files downloaded successfully.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- code/STpath pipeline/preprocess_STImage-1K4M.R
+++ code/STpath pipeline/preprocess_STImage-1K4M.R
@@ -11,8 +11,35 @@ metadata_visium <- metadata[metadata$tech == "Visium", ]
 metadata_visium$subtype <- ifelse(metadata_visium$subtype %in% c("ER", "HER2"),
                                   paste0(metadata_visium$subtype, "+"), 
                                   metadata_visium$subtype)
+metadata_visium$subtype <- ifelse(metadata_visium$subtype == "Luminal",
+                                  "ER+", 
+                                  metadata_visium$subtype)
 write.csv(metadata_visium, file = file.path(github_dir, "metadata_brca_visium.csv"),
           row.names = F)
+
+
+mtx <- readMM("/Users/zhiningsui/Library/CloudStorage/Dropbox/st2image_data/Wu_2021/data/filtered_count_matrices/CID4290_filtered_count_matrix/matrix.mtx.gz")
+mtx <- as.matrix(mtx)
+barcodes <- read.csv("/Users/zhiningsui/Library/CloudStorage/Dropbox/st2image_data/Wu_2021/data/filtered_count_matrices/CID4290_filtered_count_matrix/barcodes.tsv.gz",
+                     header = F)
+features <- read.csv("/Users/zhiningsui/Library/CloudStorage/Dropbox/st2image_data/Wu_2021/data/filtered_count_matrices/CID4290_filtered_count_matrix/features.tsv.gz",
+                     header = F)
+colnames(mtx) <- barcodes$V1
+rownames(mtx) <- features$V1
+mtx <- t(mtx)
+rownames(mtx) <- paste0("Human_Breast_Wu_06052021_Visium_CID4290_", rownames(mtx))
+write.csv(mtx, "/Users/zhiningsui/GitHub/STimage-1K4M/Visium/gene_exp/Human_Breast_Wu_06052021_Visium_CID4290_count.csv")
+
+
+tmp <- read.csv("/Users/zhiningsui/Library/CloudStorage/Dropbox/st2image_data/Wu_2021/data/spatial/CID4290_spatial/tissue_positions_list.csv", 
+                header = F)
+
+tmp <- tmp[, c(1, 5, 6)]
+tmp$V1 <- paste0("Human_Breast_Wu_06052021_Visium_CID4290_", tmp$V1)
+tmp <- tmp %>%
+  column_to_rownames("V1")
+colnames(tmp) <- c("yaxis", "xaxis")
+  
 metadata_visium$sid <- metadata_visium$slide
 sp_info <- metadata_visium[, c("sid", "subtype", "tech")]
 sp_info$dir <- file.path(st_dir, sp_info$tech)
